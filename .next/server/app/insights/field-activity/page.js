33ad8250030fcd7e416330048aa82a5d0dbfa7b2
Rizki Afrizal <rizki.afrizal02@gmail.com>
2025-06-09 15:25:02 +0700

    Revisi

--- .next/server/app/insights/field-activity/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/insights/field-activity/page.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_090a22._.js");
-runtime.loadChunk("server/chunks/[root of the server]__1ff475._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/insights/field-activity/page/actions.js (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js/(COMPONENT_0)/[project]/src/app/layout.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_1)/[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_2)/[project]/src/app/insights/field-activity/page.js [app-rsc] (ecmascript, Next.js server component) (ecmascript) {facade}", CHUNK_PUBLIC_PATH).exports;
--- .next/server/app/monitoring/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/monitoring/page.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_d29c69._.js");
-runtime.loadChunk("server/chunks/[root of the server]__ffeb02._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/monitoring/page/actions.js (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js/(COMPONENT_0)/[project]/src/app/layout.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_1)/[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_2)/[project]/src/app/monitoring/page.js [app-rsc] (ecmascript, Next.js server component) (ecmascript) {facade}", CHUNK_PUBLIC_PATH).exports;
